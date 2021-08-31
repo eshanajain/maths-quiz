cfc3ae8c20afd34f443446786896b82c2e9ef0e3
eshanajain <82397896+eshanajain@users.noreply.github.com>
2021-08-31 14:56:07 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -14,3 +14,41 @@ document.getElementById("output").innerHTML=row;
 document.getElementById("number1").value = "";
 document.getElementById("number2").value = "";
 }
+
+question_turn = "player1";
+answer_turn = "player2";
+
+function check()
+{
+
+    get_answer = document.getElementById("input_check_box").value;
+    if(get_answer == actual_answer)
+    {
+         
+        if(answer_turn == "player1")
+        {
+
+            update_player1_score = player1_score +1;
+            document.getElementById("player_score").innerhtml = update_player1_score;
+        }
+        else
+        {
+
+            update_player2_score = player2_score +1;
+            document.getElementById("player_score").innerHTML = update_player2_score;
+        }
+    }
+
+ if(question_turn == player1)
+ {
+
+    question_turn = "player2"
+    document.getElementById("player_question").innerHTML = "Question Turn - "+ player2_name ; 
+ }
+    else{
+
+        question_turn = player1
+        document_turn = "player1"
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name ;
+    }
+}
